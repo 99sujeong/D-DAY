7fa6b921ac01ef287cc085d8da0e8e522125d056
99sujeong <66475575+99sujeong@users.noreply.github.com>
2021-02-19 21:43:59 +0900

    css practice

---
+++ css/animation.js
@@ -0,0 +1,20 @@
+/*
+$(function () {
+    $('div').click(function () {
+        $(this).css('-webkit-animation-name', 'duration');
+    });
+});
+
+
+$(function () {
+    $('div').click(function () {
+        $(this).css('-webkit-animation-name', 'boxDelay');
+    });
+});
+*/
+
+$(function () {
+    $('div').click(function () {
+        $(this).css('animation-name', 'direction');
+    });
+});
