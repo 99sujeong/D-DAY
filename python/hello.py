d3030165e7363b03e73b376156a4f20fe9e5b260
99sujeong <66475575+99sujeong@users.noreply.github.com>
2021-02-20 22:02:44 +0900

    python_hello.py

---
+++ python/hello.py
@@ -0,0 +1,77 @@
+print("Hello World!")
+
+print("Mary's cosmetics")
+
+print("신씨가 소리질렀다. \"도둑이야\".")
+
+print("\"C:\Windows\"")
+
+print("안녕하세요. \n만나서\t\t반갑습니다")
+
+print("오늘은", "일요일")
+
+print("naver;kakao;sk;samsung")
+
+print("naver/kakao/sk/samsung")
+
+print("first", end="");print("second")
+
+print(5/3)
+
+삼성전자 = 50000
+총평가금액 = 10*삼성전자
+print(총평가금액)
+
+시가총액 = 298*(10**12)
+현재가 = 50000
+PER = 15.79
+
+s = "hello"
+t = "python"
+print(s+"! "+t)
+
+a = "132"
+print(type(a))
+
+num_str = "720"
+int(num_str)
+
+print(type(num_str))
+print(type(int(num_str)))
+
+num = 100
+str(num)
+
+print(str(num))
+print(type(str(num)))
+
+float("15.79")
+print(float("15.79"))
+print(type(float("15.79")))
+
+year = "2020"
+year = int(year)
+
+for i in range(3):
+    print(year)
+    year = year - 1
+
+air = 48584
+sum = air * 36
+print(sum)
+
+letters = 'python'
+print(letters[0], letters[2])
+
+license_plate = "24가 2210"
+print(license_plate[4:])
+
+string = "홀짝홀짝홀짝"
+str = string[0] + string[2] + string[4]
+print(str)
+
+string = "PYTHON"
+str = ""
+for i in range(0, len(string)):
+    str += string[(len(string)-1)-i]
+print(str)
